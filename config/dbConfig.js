4fbe6c9375a99d6f7f863444842e89de0966e1fe
saitejacm041@gmail.com <saitejacm041@gmail.com>
2026-02-17 12:11:05 +0530

    correct Railway env variable names

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -92,11 +92,11 @@
 const mysql = require("mysql2");
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: process.env.MYSQLPORT,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
@@ -105,4 +105,3 @@ const pool = mysql.createPool({
 console.log("✅ MySQL Pool Created");
 
 module.exports = pool;
-
