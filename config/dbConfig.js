bced3920ba4ac601686a100d75f42d5a5c53bddc
saitejacm041@gmail.com <saitejacm041@gmail.com>
2026-02-17 12:34:58 +0530

    fixing db variables

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -89,20 +89,35 @@
 // module.exports = db;
 
 
+// const mysql = require("mysql2");
+
+// const pool = mysql.createPool({
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_NAME,
+//   port: process.env.DB_PORT,
+//   waitForConnections: true,
+//   connectionLimit: 10,
+//   queueLimit: 0
+// });
+
+
+// console.log("✅ MySQL Pool Created");
+
+// module.exports = pool;
 const mysql = require("mysql2");
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: process.env.MYSQLPORT,
   waitForConnections: true,
   connectionLimit: 10,
-  queueLimit: 0
 });
 
-
-console.log("✅ MySQL Pool Created");
+console.log("Using host:", process.env.MYSQLHOST);
 
 module.exports = pool;
