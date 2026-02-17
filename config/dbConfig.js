dae833a5f9f4e79654873960198bdb8bf9bda8e8
saitejacm041@gmail.com <saitejacm041@gmail.com>
2026-02-17 11:39:25 +0530

    fix: switch to MySQL connection pool

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -38,22 +38,38 @@
 // module.exports = db;
 
 
+// const mysql = require("mysql2");
+
+// const db = mysql.createConnection({
+//   host: process.env.DB_HOST || "mysql.railway.internal",
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_NAME,
+//   port: process.env.DB_PORT || 3306
+// });
+
+// db.connect((err) => {
+//   if (err) {
+//     console.error("❌ DB connection failed:", err.message);
+//     return;
+//   }
+//   console.log("✅ Connected to MySQL database");
+// });
+
+// module.exports = db;
+
 const mysql = require("mysql2");
 
-const db = mysql.createConnection({
-  host: process.env.DB_HOST || "mysql.railway.internal",
+const db = mysql.createPool({
+  host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  port: process.env.DB_PORT || 3306
-});
-
-db.connect((err) => {
-  if (err) {
-    console.error("❌ DB connection failed:", err.message);
-    return;
-  }
-  console.log("✅ Connected to MySQL database");
+  port: process.env.DB_PORT,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
 });
 
 module.exports = db;
+
