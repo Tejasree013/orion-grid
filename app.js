e50b5c03c29b021de85c8bafb47021494206e951
saitejacm041@gmail.com <saitejacm041@gmail.com>
2026-02-20 10:55:57 +0530

    fixing the health check issue

--- app.js
+++ app.js
@@ -62,7 +62,10 @@ app.get('/', (req, res) => {
 
 // 🔹 START SERVER
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, '0.0.0.0', () => {
-  console.log(`Server running on port ${PORT}`);
+// app.listen(PORT, '0.0.0.0', () => {
+//   console.log(`Server running on port ${PORT}`);
+// });
+app.listen(process.env.PORT || 3000, () => {
+  console.log("Server running...");
 });
 
