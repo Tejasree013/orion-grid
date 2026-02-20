b0575f1ea48c89998fa068542224bc1211112492
saitejacm041@gmail.com <saitejacm041@gmail.com>
2026-02-20 12:11:00 +0530

    fixing app.js

--- app.js
+++ app.js
@@ -61,13 +61,19 @@ app.get('/', (req, res) => {
 });
 
 // 🔹 START SERVER
-const PORT = process.env.PORT || 3000;
-// app.listen(PORT, '0.0.0.0', () => {
+// const PORT = process.env.PORT || 3000;
+// // app.listen(PORT, '0.0.0.0', () => {
+// //   console.log(`Server running on port ${PORT}`);
+// // });
+// // app.listen(process.env.PORT || 3000, () => {
+// //   console.log("Server running...");
+// // });
+// app.listen(PORT, () => {
 //   console.log(`Server running on port ${PORT}`);
 // });
-// app.listen(process.env.PORT || 3000, () => {
-//   console.log("Server running...");
-// });
-app.listen(PORT, () => {
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on port ${PORT}`);
 });
