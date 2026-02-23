f5215e6021ed158bd3846688c6129ebe97eb5aee
saitejacm041@gmail.com <saitejacm041@gmail.com>
2026-02-23 12:15:17 +0530

    chhanging app.js

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ const fileUpload = require('express-fileupload');
 const fetchCourses = require('./routes/fetchCourses');
 const fetchWorkshops = require('./routes/fetchWorkshops');
 const fetchProject = require('./routes/fetchProject');
-const fetchInternship = require('./routes/fetchInternships');
+const fetchInternships = require('./routes/fetchInternships');
 
 const app = express();
 
@@ -53,7 +53,7 @@ app.use(express.static('public'));
 app.use('/api/fetch', fetchCourses);
 app.use('/api/workshop', fetchWorkshops);
 app.use('/api/project',fetchProject);
-app.use('/api/internship', fetchInternships);
+app.use('/api/internship',fetchInternships);
 
 // 🔹 HEALTH CHECK (Railway needs this)
 // app.get('/health', (req, res) => {
